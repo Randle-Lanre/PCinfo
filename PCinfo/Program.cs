7c720fbb3721347a4bd8631a2f852c75886cd707
Randle-lanre <kennyrandle09@hotmail.co.uk>
2022-10-02 17:58:11 +0100

    feat: feature complete

--- PCinfo/Program.cs
+++ PCinfo/Program.cs
@@ -4,10 +4,14 @@ using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using Spectre.Console;
+using System.Diagnostics;
+
 
 var pcName = Environment.MachineName;
 var username = Environment.UserName;
 var os = Environment.OSVersion.ToString();
+
+
 
 #region DNS detials
 
@@ -69,7 +73,60 @@ static string GetUpTime()
 static extern ulong GetTickCount64();
 
 
-var uptime = GetUpTime();
+
+
+string uptime = GetUpTime();
+
+
+
+#endregion
+
+#region CPU and RAM
+
+PerformanceCounter cpuCounter;
+PerformanceCounter ramCounter;
+#pragma warning disable
+cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+
+
+float CupValue()
+{
+    cpuCounter.NextValue();
+    Thread.Sleep(500);
+   return cpuCounter.NextValue();
+}
+
+
+
+#endregion
+
+
+#region Free drive space
+
+long GetTotalFreeSpace(string driveName = "C:\\")
+{
+    foreach (DriveInfo drive in DriveInfo.GetDrives())
+    {
+        if (drive.IsReady && drive.Name == driveName)
+        {
+            return drive.AvailableFreeSpace;
+        }
+    }
+    return -1;
+}
+
+
+//--
+ static string FormatFileSize(long bytes)
+{
+    var unit = 1024;
+    if (bytes < unit) { return $"{bytes} B"; }
+
+    var exp = (int)(Math.Log(bytes) / Math.Log(unit));
+    return $"{bytes / Math.Pow(unit, exp):F2} {("KMGTPE")[exp - 1]}B";
+}
+
 
 
 
@@ -85,20 +142,7 @@ table.AddColumn("InfoType");
 table.AddColumn(new TableColumn("Value").Centered());
 
 
-//row setup
 
-table.AddRow(new Markup("PC name "), new Markup($"[green] {pcName} [/]"));
-table.AddRow(new Markup("Username"), new Markup($"[green] {username} [/]"));
-table.AddRow(new Markup("Wifi"), new Markup($"[green]{firstOrDefaultWirelessAddress} [/]"));
-table.AddRow(new Markup("LAN"), new Markup($"[green]{firstOrDefaultLan} [/]"));
-// table.AddRow(new Markup("My IP address"), new Markup("[green] 0.0.0.0[/]"));
-table.AddRow(new Markup("connected"), new Markup($"[green] {connected} [/]"));
-// table.AddRow(new Markup("UP Time"), new Markup("[green] 3hrs 45 mins [/]"));
-table.AddRow(new Markup("Uptime"), new Markup($"[green]{uptime}[/]"));
-table.AddRow(new Markup("Used Storage Space"), new Markup("[green]Available storage space is 25% [/]"));
-table.AddRow(new Markup("Ram usage"), new Markup("[green] 60% [/]"));
-table.AddRow(new Markup("CPU usage"), new Markup("[green] 60% [/]"));
-table.AddRow(new Markup("Window version"), new Markup($"[green] {os} [/]"));
 
 
 //change table apearance
@@ -106,8 +150,34 @@ table.Border(TableBorder.Rounded);
 table.BorderColor(Color.Yellow);
 table.Columns[1].PadLeft(3).PadRight(6);
 table.Centered();
+table.Collapse();
 
 
 Console.WriteLine("\n");
-AnsiConsole.Write(table);
-Console.ReadKey();
+// AnsiConsole.Write(table);
+
+  AnsiConsole.Live(table).Start( x =>
+{
+//row setup
+
+    table.AddRow(new Markup("PC name "), new Markup($"[green] {pcName} [/]"));
+    table.AddRow(new Markup("Username"), new Markup($"[green] {username} [/]"));
+    table.AddRow(new Markup("Wifi"), new Markup($"[green]{firstOrDefaultWirelessAddress} [/]"));
+    table.AddRow(new Markup("LAN"), new Markup($"[green]{firstOrDefaultLan} [/]"));
+
+    table.AddRow(new Markup("connected"), new Markup($"[green] {connected} [/]"));
+
+    
+    table.AddRow(new Markup("Uptime"), new Markup($"[green]{uptime}[/]"));
+
+    table.AddRow(new Markup("Used Storage Space"), new Markup($"[green]Available storage space is {FormatFileSize(GetTotalFreeSpace())} [/]"));
+    table.AddRow(new Markup("Ram usage"), new Markup($"[green] {ramCounter.NextValue()} MB [/]"));
+    table.AddRow(new Markup("CPU usage"), new Markup($"[green] {Math.Round(CupValue(), 1)} % [/]"));
+    table.AddRow(new Markup("Window version"), new Markup($"[green] {os} [/]"));
+    x.Refresh();
+    Thread.Sleep(1000);
+});
+
+
+
+// Console.ReadLine();
